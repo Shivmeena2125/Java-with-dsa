80cfe3a6c7fb1ded9b0c651bf623c1683756f859
Shivmeena5785-creat <shivmeena5785@gmail.com>
2026-05-27 10:12:26 +0530

    functions

---
+++ fifthclass.java
@@ -0,0 +1,70 @@
+import java.util.*;
+public class fifthclass{
+    // Function..
+    // public static void printMyName(String name){
+    //     System.out.println(name);
+    //     return;
+    // }
+
+    // make a function to add two numbers and return the sum..
+    // public static int sum(int a, int b){
+    //     return a+b;
+    // }
+
+
+    //make a function to multiple two numbers and returen the product..
+    // public static int product(int a,int b){
+    //     return a*b;
+    // }
+
+    // make a function to calculate the factorial on any number..
+    // public static void factorial(int n){
+    //     if(n<0){
+    //         System.out.println("Invalid number..");
+    //         return;
+    //     }
+    //      int Factorial = 1;
+    //     for(int i=n; i>=1; i--){
+    //         Factorial *=i;
+    //     }
+    //      System.out.println(Factorial);
+
+    // }
+
+
+    //
+
+    
+
+    public static void main(String []args){
+        // Function calling..
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your name: ");
+        // String name= sc.nextLine();
+        // printMyName(name);
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc . nextInt();
+        // int b = sc . nextInt();
+        // System.out .println("the sum of two number is:" +sum(a,b));
+
+        // Scanner sc =new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b =sc .nextInt();
+        // System.out.println("the product of two numbers is:" + product(a,b));
+
+
+        // Scanner sc =new Scanner(System.in);
+        // int n = sc.nextInt();
+        // factorial(n);
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
