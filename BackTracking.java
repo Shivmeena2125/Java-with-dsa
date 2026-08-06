7bef4bd97a9b31ee470a4fcad3072ec9dc8cebee
Shivmeena5785-creat <shivmeena5785@gmail.com>
2026-08-06 11:41:51 +0530

    backtracking few questions

--- BackTracking.java
+++ BackTracking.java
@@ -179,6 +179,74 @@ public class BackTracking {
  }
 
 
+ //Sudoku Solver
+ 
+ public void solveSudoku(char [][]board){
+    helper(board,0,0);
+
+ }
+     public boolean isSafe(char[][] board,int row,int col,int number){
+    //row&col
+    for(int i=0;i<board.length;i++){
+        if(board[i][col]==(char)(number +'0')){
+            return false;
+        }if(board[row][i]==(char)(number+'0')){
+            return false;
+
+        }
+    }
+    //grid
+    int sr= (row/3)*3;
+    int sc= (col/3)*3;
+    for(int i=sr; i<sr+3;i++){
+        for(int j=sc;j<sc+3; j++){
+            if(board[i][j]==(char)(number+'0')){
+                return false;
+            }
+        }
+    }
+    return true;
+
+ }
+
+
+ public boolean helper(char[][]board,int row,int col){
+  if(row==board.length){
+    return true;
+  }
+
+    int newrow=0;
+    int newcol=0;
+    if(col != board.length-1){
+        newrow =row;
+        newcol =col+1;
+    }else{
+        newrow = row +1;
+        newcol= 0;
+    }
+    if(board[row][col] != '.'){
+        if(helper(board,newrow,newcol)){
+            return true;
+        }
+    }
+        else{
+            for(int i =1; i<=9;i++){
+                if(isSafe(board,row,col,i)){
+                    board[row][col]=(char)(i+'0');
+                    if(helper(board,newrow,newcol))
+                        return true;
+                    
+                    else
+                        board[row][col]='.';
+                    
+                
+            }
+        }
+    }
+    return false;
+ }
+
+
 
     public static void main(String [] arg){
         System.out.println("Starting...");
@@ -188,16 +256,24 @@ public class BackTracking {
 
 
     // n Queens problem
-    int n =4;
-    char [][] board = new char[n][n];
-    for(int i =0; i<board.length; i++){
-        for (int j =0; j<board.length; j++){
-            System.out.print(board[i][j]+" ");
-        }
-        System.out.println();
-    }
+    // int n =4;
+    // char [][] board = new char[n][n];
+    // for(int i =0; i<board.length; i++){
+    //     for (int j =0; j<board.length; j++){
+    //         System.out.print(board[i][j]+" ");
+    //     }
+    //     System.out.println();
+    // }
 
-    nQueens(0,board);
+    // nQueens(0,board);
+
+
+    //Sudoku 
+    
+    
+
+
+
 
     }
     
